c19fde746a9886f00decda168a8044ffb3eb59b8
syr <syr@adobe.com>
2023-06-22 17:11:08 +0530

    Scroll fixes

--- blocks/cardcarousel/cardcarousel.js
+++ blocks/cardcarousel/cardcarousel.js
@@ -1,99 +1,109 @@
+import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
+
 export default function decorate(block) {
-
-
-
-  const outerDiv = document.createElement('div');
-  outerDiv.classList.add("outerdiv");
+  
+  const ul = document.createElement('div');
+  ul.classList.add("outerdiv");
   var num_items = block.children.length;
   var current = 1;
 
-
   [...block.children].forEach((row) => {
-    const innerDiv = document.createElement('div');
-    innerDiv.classList.add("innerdiv");
-    innerDiv.innerHTML = row.innerHTML;
-    innerDiv.style.order = current;
-    innerDiv.dataset.position = current;
+    const li = document.createElement('div');
+    li.classList.add("innerdiv");
+    li.innerHTML = row.innerHTML;
+    li.style.order = current;
+    li.dataset.position = current;
     current++;
-    [...innerDiv.children].forEach((div) => {
+    [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
-    outerDiv.append(innerDiv);
-  });
 
-  current=0;
-  var isRightClick;
+
+    ul.append(li);
+  });
+  current=1;
+  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
+
+
+  var next = function (event) {
+    var isRight = event.target.className === "scrollRight";
+    var outerdiv = event.target.parentElement.getElementsByClassName("outerdiv")[0];
+    if (isRight) {
+      outerdiv.classList.add('slide-transition-right');
+      outerdiv.style.transform = 'translate3d(-25%, 0px,0px)';
+    } else {
+      outerdiv.classList.add('slide-transition-left');
+      outerdiv.style.transform = 'translate3d(25%, 0px,0px)';
+    }
+  };
 
   const buttonl = document.createElement("button");
   buttonl.classList.add("scrollLeft");
-  buttonl.innerText = "Left";
-  buttonl.addEventListener("click", function() {
-    next(false);
-  });
+  buttonl.innerText = "<";
+  buttonl.addEventListener("click", next, this);
   block.append(buttonl);
 
-  block.append(outerDiv);
-
-  const button = document.createElement("button");
-  button.classList.add("scrollRight");
-  button.innerText = "Right";
-  button.addEventListener("click", function() {
-    next(true);
-  });
-  block.append(button);
+  block.append(ul);
 
 
-  document.querySelector(".outerdiv").addEventListener("transitionend", () => {
-    changeOrder();
-});
-
-  var next = function (isRight) {
-    document.querySelector(".outerdiv").classList.add('slide-transition');
-    if (isRight) {
-      isRightClick = true;
-      document.querySelector(".outerdiv").style.transform = 'translate3d(-25%, 0px,0px)';
+  var changeOrder = function (event) {
+    
+    var outerdiv = event.target.parentElement.getElementsByClassName("outerdiv")[0];
+    var isRight = outerdiv.classList.contains("slide-transition-right");
+    
+    if(isRight) {
+      if(current === num_items) {
+          current = 1;
+      } else {
+          current++;
+      }
     } else {
-      isRightClick = false;
-      document.querySelector(".outerdiv").style.transform = 'translate3d(25%, 0px,0px)';
+      if(current == 1) {
+        current = num_items;
+      } else {
+        current--;
+      }
     }
-  };
-
-  var changeOrder = function () {
-    if(isRightClick === undefined) {
-      return;
-    }
-    if(isRightClick) {
-      let currItem = ((current%num_items) + num_items)%num_items + 1;
-      document.querySelector(".innerdiv[data-position='" + currItem +"']").style.order = current+num_items+1;
-      current++;
-    } else {
-      let currItem = (((current-1)%num_items)+ num_items)%num_items +1;
-      document.querySelector(".innerdiv[data-position='" + currItem +"']").style.order = current;
-      current--;
-    }
-    /*
-    if(current === num_items) {
-      current = 1;
-    } else {
-      current++;
-    }
-
 
     let order = 1;
+
     for( let i = current; i <= num_items; i++) {
-      document.querySelector(".innerdiv[data-position='" + i +"']").style.order = order;
-      order++;
+        outerdiv.querySelector(".innerdiv[data-position='" + i +"']").style.order = order;
+        order++;
     }
 
     for( let i = 1 ; i < current; i++) {
-      document.querySelector(".innerdiv[data-position='" + i +"']").style.order = order;
-      order++;
-    }*/
-    document.querySelector(".outerdiv").classList.remove('slide-transition');
-    document.querySelector(".outerdiv").style.transform = 'translate3d(0px, 0px,0px)'
-  };
+        outerdiv.querySelector(".innerdiv[data-position='" + i +"']").style.order = order;
+        order++;
+    }
+
+    outerdiv.classList.remove('slide-transition-right');
+    outerdiv.classList.remove('slide-transition-left');
+    outerdiv.style.transform = 'translate3d(0px, 0px,0px)'
+};
+
+  const button = document.createElement("button");
+  button.classList.add("scrollRight");
+  button.innerText = ">";
+  button.addEventListener("click", next, this);
+  block.append(button);
+
+  block.querySelector(".outerdiv").addEventListener("transitionend", (event) => {
+    changeOrder(event);
+}); 
+
+var blockParent = block.parentElement;
+var title =  block.parentElement.parentElement.dataset.title;
+
+
+var heading = document.createElement("h3");
+heading.classList.add("carouselheading")
+heading.innerText = title;
+blockParent.textContent = '';
+blockParent.append(heading);
+blockParent.append(block);
 
 
 }
